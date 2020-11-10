b1519992ec87d6548a889f504b2190d3d05adf82
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-11-10 04:52:34 +0000

    Update Upload-Anon.psm1

--- Automation, Monitoring, and Auditing/Upload-Anon.psm1
+++ Automation, Monitoring, and Auditing/Upload-Anon.psm1
@@ -3,11 +3,13 @@ param(
 [string]$File 
 )
 If (!$File){
-    Write-Host "Please proide the a file. Ex: Upload-Anon -File 'C:\temp\test.txt'"
+    Write-Host "Please provide the a file. Ex: Upload-Anon -File 'C:\temp\test.txt'"
 }Else {
-Invoke-WebRequest -Method Post -Uri https://api.anonfiles.com/upload -Form @{file=(Get-Item $File)}
+Invoke-WebRequest -Method "Post" -Uri "https://api.anonfiles.com/upload" -Form @{file=(Get-Item $File)}
 }
-)
 }
+
+
+
 
 
