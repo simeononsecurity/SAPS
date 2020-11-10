3661ff63caed715f01b60249ef16252b1eebefb3
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-11-10 07:20:18 +0000

    Update Upload-Anon.psm1

--- Automation, Monitoring, and Auditing/Upload-Anon.psm1
+++ Automation, Monitoring, and Auditing/Upload-Anon.psm1
@@ -7,7 +7,7 @@ If (!$File){
     Write-Host "Please provide the a file. Ex: Upload-Anon -File 'C:\temp\test.txt'"
 }Else {
     Write-Host "Please wait wile the file is uploaded"
-    (Invoke-WebRequest -Method "Post" -Uri "https://api.anonfiles.com/upload" -Form @{file=(Get-Item $File)}).content  -Split {$_ -eq '"' -or $_ -eq "{" -or $_ -eq "}" -or $_ -eq ',' -or $_ -eq ' '} | Select-String -NoEmphasis -Pattern 'https' |  Out-String | Set-Variable -Name links
+    (Invoke-WebRequest -Method "Post" -Uri "https://api.anonfiles.com/upload" -Form @{file=(Get-Item $File)}).content -Split {$_ -eq '"' -or $_ -eq "{" -or $_ -eq "}" -or $_ -eq ',' -or $_ -eq ' '} | Select-String -NoEmphasis -Pattern 'https' |  Out-String | Set-Variable -Name links
     Write-Host "`n"
     Write-Host "Share your files with either of the following links:"
     Write-Host "$links"
