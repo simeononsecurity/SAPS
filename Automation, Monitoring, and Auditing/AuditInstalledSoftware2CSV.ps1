2d3f32442f29e81bfe33a096a3d8b4df8b44c51a
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2021-01-28 18:46:44 +0000

    Update AuditInstalledSoftware2CSV.ps1

--- Automation, Monitoring, and Auditing/AuditInstalledSoftware2CSV.ps1
+++ Automation, Monitoring, and Auditing/AuditInstalledSoftware2CSV.ps1
@@ -1,19 +1,29 @@
 #https://github.com/simeononsecurity/SAPS
 
-## Sets Environment Variables ##
-$computername = "$env:computername"
-$timestamp = "$(((get-date).ToUniversalTime()).ToString("yyyyMMddTHHmmssZ"))"
-$scriptname = "AuditInstalledSoftware2CSV"
-$file = "$scriptname-$computername-$timestamp.csv"
-$outputpath = "C:\temp\Scripts\$scriptname"
-$csvoutfile = "$path\$file"
+##Install and Import AD Modules For Powershell v6
+Install-Module -Name WindowsCompatibility -Force
+Import-Module -Name WindowsCompatibility -Force
+Import-WinModule -Name ActiveDirectory -Force
 
+#Install and Import Powershell AD Module
+Install-WindowsFeature RSAT-AD-PowerShell -Force
+Import-Module ActiveDirectory -Force
 
-## Creates Output Folder Structure if it Doesn't Exist ##
-If(!(test-path $outputpath))
-{
-      New-Item -ItemType Directory -Force -Path $outputpath
+$computerlist = Get-ADComputer -Filter {Enabled -eq $True}
+$finaldestination= Read-Host "Specify a Network Path to Copy Files to When Done"
+
+ForEach ($computer in ($computerlist).Name){
+    Invoke-Command -Computer $computer -ScriptBlock {
+        $computername = "$env:computername"
+        $timestamp = "$(((get-date).ToUniversalTime()).ToString("yyyyMMddTHHmmssZ"))"
+        $scriptname = "AuditInstalledSoftware2CSV"
+        $file = "$scriptname-$computername-$timestamp.csv"
+        $outputpath = "C:\temp\Scripts\$scriptname"
+        $csvoutfile = "$path\$file"
+        If(!(test-path $outputpath)){
+            New-Item -ItemType Directory -Force -Path $outputpath
+        }
+        Get-WmiObject -Namespace ROOT\CIMV2 -Class Win32_Product  | Select-Object Installdate, Name, Version, Vendor, PSComputerName | Sort-Object Installdate | Export-CSV $csvoutfile -NoTypeInfo
+    }
+    Copy-Item \\"$computer"\c$\temp\Scripts\AuditInstalledSoftware2CSV\*.csv $finaldestination
 }
-
-## Collects Installed Application and Software Information, Organises Information, & Saves Information into a Sorted CSV (EXCEL) File ##
-Get-WmiObject -Namespace ROOT\CIMV2 -Class Win32_Product  | Select-Object Installdate, Name, Version, Vendor, PSComputerName | Sort-Object Installdate | Export-CSV $csvoutfile -NoTypeInfo
